24b7e0f0898ef947bdbdcc12a4c92881266fdcf3
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-03-29 20:01:29 +0100

    fix: update the bind in main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ elif framing == 'osc10':
 
 if transport == 'tcp':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9000))
+    server_socket.bind(('0.0.0.0', 9000))
     server_socket.listen(1)
     print("TCP server listening on port 9000")
     ## When a client connects, accept the connection and return 1 million osc messages as fast as possible.
